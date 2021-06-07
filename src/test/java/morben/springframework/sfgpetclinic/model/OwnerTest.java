82e3663770c520ea3b9271fda25b140ee7912210
OrbenMichele <micheleorben@gmail.com>
2021-06-06 21:50:37 -0300

    Parameterized tests: display name

--- src/test/java/morben/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/morben/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,6 +1,6 @@
 package morben.springframework.sfgpetclinic.model;
 
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -31,7 +31,8 @@ class OwnerTest implements ModelTests{
         assertThat(owner.getCity(), is("Floripa")); //hamcrest
     }
 
-    @ParameterizedTest
+    @DisplayName("Value Source Test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @ValueSource(strings = {"Spring", "Famework", "morben"})
     void testValueSouce(String val) {
         System.out.println(val);
