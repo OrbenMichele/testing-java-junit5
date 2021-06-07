7b094370b6847f3b63149616f83e271883be6c50
OrbenMichele <micheleorben@gmail.com>
2021-06-06 21:05:03 -0300

    JUnit Default Tests Methods

--- src/test/java/morben/springframework/sfgpetclinic/controllers/ControllersTest.java
+++ src/test/java/morben/springframework/sfgpetclinic/controllers/ControllersTest.java
@@ -1,11 +1,19 @@
 package morben.springframework.sfgpetclinic.controllers;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * Created by micheleorben on 6/6/21
  */
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controllers")
 public interface ControllersTest {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Lets do smt here");
+    }
 }
