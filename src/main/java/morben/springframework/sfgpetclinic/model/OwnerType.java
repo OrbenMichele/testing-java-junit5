e81c5a900667c85e5b87776ef5edc48ba0685761
OrbenMichele <micheleorben@gmail.com>
2021-06-08 20:39:44 -0300

    Parameterized tests: enum source

---
+++ src/main/java/morben/springframework/sfgpetclinic/model/OwnerType.java
@@ -0,0 +1,10 @@
+package morben.springframework.sfgpetclinic.model;
+
+/**
+ * Created by micheleorben on 6/8/21
+ */
+
+public enum OwnerType {
+
+    INDIVIDUAL, COMPANY
+}
--- src/test/java/morben/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/morben/springframework/sfgpetclinic/model/OwnerTest.java
@@ -3,6 +3,7 @@ package morben.springframework.sfgpetclinic.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -36,5 +37,12 @@ class OwnerTest implements ModelTests{
     @ValueSource(strings = {"Spring", "Famework", "morben"})
     void testValueSouce(String val) {
         System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
     }
 }
