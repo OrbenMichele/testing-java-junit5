1b6433edac33f954428e8bd935555a9886b4db3e
OrbenMichele <micheleorben@gmail.com>
2021-06-08 20:56:53 -0300

    Method Provider test

--- src/test/java/morben/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/morben/springframework/sfgpetclinic/model/OwnerTest.java
@@ -3,10 +3,9 @@ package morben.springframework.sfgpetclinic.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -64,5 +63,19 @@ class OwnerTest implements ModelTests{
     @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
     void csvFromFileSourceTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " - " + val1 + ":" + val2);
+    }
+
+    @DisplayName("Method Provider Test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void fromMethodProviderTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " - " + val1 + ":" + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 14, 24),
+                Arguments.of("MI", 12, 23)
+        );
     }
 }
