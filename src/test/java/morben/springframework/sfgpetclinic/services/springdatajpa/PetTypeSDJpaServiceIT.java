bff8c7321f877d80a831932f53fc10eaf98efef5
OrbenMichele <micheleorben@gmail.com>
2021-06-08 21:07:31 -0300

    Integration Maven test

---
+++ src/test/java/morben/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,36 @@
+package morben.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//IT = Integration Test
+//to maven execute that differently
+
+class PetTypeSDJpaServiceIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
