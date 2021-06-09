28074b4a5ad653f93e5313cbc55fbf1f869d851b
OrbenMichele <micheleorben@gmail.com>
2021-06-08 22:31:40 -0300

    JUnit Extension tests

--- src/test/java/morben/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/morben/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -1,13 +1,15 @@
 package morben.springframework.sfgpetclinic.services.springdatajpa;
 
+import morben.springframework.sfgpetclinic.junitextensions.TimingExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 //IT = Integration Test
 //to maven execute that differently
-
+@ExtendWith(TimingExtension.class)
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
